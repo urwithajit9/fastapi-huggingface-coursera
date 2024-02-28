f4c91ca75e978310394af775ee9926ff2538495b
Ajit Kumar <urwithajit9@gmail.com>
2024-02-28 14:55:14 +0000

    fastapi app code and docker file

---
+++ webapp/main.py
@@ -0,0 +1,20 @@
+from transformers import pipeline
+from fastapi import FastAPI 
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+
+generator = pipeline('text-generation', model='gpt2')
+
+app = FastAPI()
+
+class Body(BaseModel):
+    text:str
+
+@app.get('/')
+def root():
+    return HTMLResponse("<h1>Hello </h1>")
+
+@app.post('/generate')    
+def predict(body:Body):
+    results = generator(body.text,max_length=35,num_return_sequences=1)
+    return results[0]
